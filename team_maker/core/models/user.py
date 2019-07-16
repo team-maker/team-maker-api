fe59c6c01471375fed769d901818296834a8032c
Rodrigo Dinis <rodrigo.f.dinis@gmail.com>
2019-07-16 20:19:01 +0100

    model-user 5?

--- team_maker/core/models/user.py
+++ team_maker/core/models/user.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return "{}".format(self.email)
