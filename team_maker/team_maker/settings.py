26b642e325af9ffe835d0f4021ddf9c638392817
Rodrigo Dinis <rodrigo.f.dinis@gmail.com>
2019-07-17 22:34:01 +0100

    create db

--- team_maker/team_maker/settings.py
+++ team_maker/team_maker/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'team_maker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'DATABASE_URL': 'postgres:///team_maker_api',
+        'ATOMIC_REQUESTS': True,
+        'NAME': 'team_maker_api'
     }
 }
 
