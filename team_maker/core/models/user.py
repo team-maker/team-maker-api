ad5b5b555286cd36980efffca1e2b677f13be3de
Rodrigo Dinis <rodrigo.f.dinis@gmail.com>
2019-07-20 15:38:18 +0100

    player.py

--- team_maker/core/models/user.py
+++ team_maker/core/models/user.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
@@ -14,18 +13,3 @@ class User(AbstractUser):
 
 def __str__(self):
     return "{}".format(self.email)
-
-
-class UserPlayer(models.Model):
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    points = models.IntegerField(default=0)
-    exp = Experiencia
-    photo = models.ImageField(upload_to='uploads', blank=True)
-
-
-class Experiencia(Enum):
-    AMADOR = 0
-    FEDERADO = 1000
-    SEMI_PROFISSIONAL = 2000
-    PROFISSIONAL = 3000
