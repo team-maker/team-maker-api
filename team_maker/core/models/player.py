0c55d4048715055b2ca45e63aa37bc8a382b2e7e
Rodrigo Dinis <rodrigo.f.dinis@gmail.com>
2019-07-20 15:40:40 +0100

    google tradutor

--- team_maker/core/models/player.py
+++ team_maker/core/models/player.py
@@ -3,15 +3,15 @@ from django.db import models
 from django.conf import settings
 
 
-class UserPlayer(models.Model):
+class Player(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
     points = models.IntegerField(default=0)
-    exp = Experiencia
+    experience = Experience
     photo = models.ImageField(upload_to='uploads', blank=True)
 
 
-class Experiencia(Enum):
+class Experience(Enum):
     AMADOR = 0
     FEDERADO = 1000
     SEMI_PROFISSIONAL = 2000
