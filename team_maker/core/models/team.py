abf968f1235a35f06c12f0641944702d709bf033
Rodrigo Dinis <rodrigo.f.dinis@gmail.com>
2019-07-26 00:09:00 +0100

    update team-py
    ver:
    Team(models.Model):
    users=[]

--- team_maker/core/models/team.py
+++ team_maker/core/models/team.py
@@ -1,15 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
 
-class Team(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-    photo = models.ImageField(upload_to='uploads', blank=True)
+class Team(models.Model):
+    name = models.CharField(max_length=130)
+    users = []
 
     def __str__(self):
-        return "{}".format(self.email)
+        return "{}".format(self.name)
