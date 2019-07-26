cac4a0d819e31f18e19c46ab6f13a2c9b9059da1
Rodrigo Dinis <rodrigo.f.dinis@gmail.com>
2019-07-26 03:30:50 +0100

    melhoria de codigo

--- team_maker/core/models/player.py
+++ team_maker/core/models/player.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import Team
 
 
 class Player(models.Model):
@@ -9,9 +10,20 @@ class Player(models.Model):
     points = models.IntegerField(default=0)
     experience = Experience
 
+    team = models.ForeignKey(user, Team, related_name='team')
+    # related_name='team'?
+
 
 class Experience(Enum):
     AMATEUR = 0
     FEDERATE = 1000
     SEMI_PROFESSIONAL = 2000
     PROFESSIONAL = 3000
+
+
+# exemplo
+# class Article(models.Model):
+#    headline = models.CharField(max_length=100)
+#    pub_date = models.DateField()
+#    reporter = models.ForeignKey(
+#        User, on_delete=models.CASCADE, related_name='reporter')
--- team_maker/core/models/team.py
+++ team_maker/core/models/team.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import Player
 
 
 class Team(models.Model):
     name = models.CharField(max_length=130)
-    users = []
+    admin = models.OneToOneField(
+        Player, on_delete=models.CASCADE, primary_key=True,)
 
     def __str__(self):
         return "{}".format(self.name)
