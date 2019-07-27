4f75ea4b9557cc9a0b323d3214a3ef576eadd98e
Rodrigo Dinis <rodrigo.f.dinis@gmail.com>
2019-07-27 10:10:20 +0100

    experience upgrade

--- team_maker/core/models/player.py
+++ team_maker/core/models/player.py
@@ -7,22 +7,16 @@ class Player(models.Model):
     player = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='player')
     points = models.IntegerField(default=0)
-    experience = Experience
+    experience = models.CharField(
+        choices=[(Experience.AMATEUR, ('AMATEUR')), (Experience.FEDERATE, ('FEDERATE')),
+                 (Experience.SEMI_PROFESSIONAL, ('SEMI_PROFESSIONAL')), (Experience.PROFESSIONAL, ('PROFESSIONAL'))])
 
     def __str__(self):
         return "{}".format(self.player)
 
 
-class Experience(Enum):
-    AMATEUR = 0
-    FEDERATE = 1000
-    SEMI_PROFESSIONAL = 2000
-    PROFESSIONAL = 3000
-
-
-# exemplo
-# class Article(models.Model):
-#    headline = models.CharField(max_length=100)
-#    pub_date = models.DateField()
-#    reporter = models.ForeignKey(
-#        User, on_delete=models.CASCADE, related_name='reporter')
+class Experience(enu.Enum):
+    AMATEUR = 'AMATEUR'
+    FEDERATE = 'FEDERATE'
+    SEMI_PROFESSIONAL = 'SEMI_PROFESSIONAL'
+    PROFESSIONAL = 'PROFESSIONAL'
