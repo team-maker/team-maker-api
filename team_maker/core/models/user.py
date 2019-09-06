8de88b0a5807f7026e6ea414da3f930bceddd9c3
RubenSousaDinis <Ruben.sousa.dinis@gmail.com>
2019-09-06 11:34:50 +0100

    48 - change user manager

--- team_maker/core/models/user.py
+++ team_maker/core/models/user.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import ugettext_lazy as _
 from .player import Player
 
 
-class UserManager(models.Manager):
+class CustomUserManager(UserManager):
     def create(self, *args, **kwargs):
-        user = super(UserManager, self).create(**kwargs)
+        user = super(CustomUserManager, self).create(**kwargs)
         Player.objects.create(user=user)
         return user
 
@@ -18,7 +18,7 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['first_name', 'last_name']
     photo = models.ImageField(blank=True, upload_to='uploads')
 
-    objects = UserManager()
+    objects = CustomUserManager()
 
     def __str__(self):
         return "{}".format(self.email)
