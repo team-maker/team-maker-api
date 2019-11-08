fcfb0bf2b5318d8f176aa454c34d5c67d44e56b0
RubenSousaDinis <Ruben.sousa.dinis@gmail.com>
2019-11-08 13:35:16 +0000

    fix rule query

--- team_maker/core/services/games.py
+++ team_maker/core/services/games.py
@@ -62,7 +62,7 @@ def distribute_game_points(game):
     team_rules = models.TeamRule.objects.filter(team=team)
     for team_rule in team_rules:
         generate_rule_points(team_rule, game)
-    mvp_rule = team_rules.get(rule_type='game_mvp')
+    mvp_rule = team_rules.get(rule__rule_type='game_mvp')
     generate_game_mvp_points(mvp_rule, game)  
     for team_group_player in game.game_team_group_players():
         team_group_player.recalculate_points_amount()
